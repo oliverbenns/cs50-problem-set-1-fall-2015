2a3c11a0aeec586c044b0c9d29ac35e552e6db72
oliverbenns <oliverbenns@gmail.com>
2016-05-08 17:15:58 +1000

    Add basic water script with user input

---
+++ water.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int showerLength;
+
+	printf("How long is your average shower in minutes?\n");
+	scanf("%d", &showerLength);
+	printf("Your shower is %d minutes long.\n", showerLength);
+}
