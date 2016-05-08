4061183d7059610152830f927a3db6080527b855
oliverbenns <oliverbenns@gmail.com>
2016-05-08 16:11:00 +1000

    Add helper bash script to compile, run and check code

---
+++ script.sh
@@ -0,0 +1,32 @@
+# Shouldn't hardcode this.
+alias check50="node /usr/local/lib/check50/SOURCES/opt/check50/bin/check50.js"
+
+# Args
+COMMAND=$1
+PROJECT=$2
+
+if [ -z "$COMMAND" ] || [ -z "$PROJECT" ];
+then
+	echo "[command] [project]"
+	exit
+fi
+
+if [ ! -f "${PROJECT}.c" ];
+then
+	echo "Project '${PROJECT}' does not exist"
+	exit
+fi
+
+case "$COMMAND" in
+"run")
+		clang -o "${PROJECT}.out" "${PROJECT}.c"
+		"${PWD}/${PROJECT}.out"
+		;;
+"check")
+		check50 "2015.fall.hacker1.${PROJECT}" "${PROJECT}.c"
+		;;
+*)
+		echo "Unknown command"
+		exit
+		;;
+esac
