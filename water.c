5ba3f9394a332a1f97e83f03b5e687acc947f67b
oliverbenns <oliverbenns@gmail.com>
2016-05-13 09:52:42 +1000

    Add water script

--- water.c
+++ water.c
@@ -1,10 +1,30 @@
+#include <cs50.h>
 #include <stdio.h>
 
-int main(void)
-{
+int getShowerLength(void);
+int calculateBottleCount(int minutes);
+
+int main(void) {
+	int showerLength = getShowerLength();
+
+	printf("bottles: %d \n", calculateBottleCount(showerLength));
+}
+
+int getShowerLength(void) {
 	int showerLength;
 
-	printf("How long is your average shower in minutes?\n");
-	scanf("%d", &showerLength);
-	printf("Your shower is %d minutes long.\n", showerLength);
+	while (showerLength <= 0) {
+		printf("minutes: ");
+		showerLength = GetInt();
+	}
+
+	return showerLength;
+}
+
+int calculateBottleCount(int minutes) {
+	float gallonsPerMinute = 1.5;
+	int gallonsInOunces = 128;
+	int bottleInOunces = 16;
+
+	return (minutes * (gallonsInOunces * gallonsPerMinute)) / bottleInOunces;
 }
