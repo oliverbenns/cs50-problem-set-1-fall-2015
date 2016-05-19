4e68ad0a2580c3c601f21a9bdb822d45ff1f765f
oliverbenns <oliverbenns@gmail.com>
2016-05-19 20:28:20 +1000

    Add mario script

--- mario.c
+++ mario.c
@@ -1,10 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
-{
-	printf("Height: ");
-	int pyramidHeight = GetInt();
+int main(void) {
+	int pyramidHeight;
 
-	printf("A half-pyramid will be generated %i blocks high\n", pyramidHeight);
+	do {
+		printf ("Height: ");
+		pyramidHeight = GetInt();
+	} while (pyramidHeight < 0 || pyramidHeight > 23);
+
+	for (int row = 1; row <= pyramidHeight; row++) {
+
+		for (int k = 0; k < (pyramidHeight - row); k++)
+			printf(" ");
+
+		for (int j = 0; j < row; j++)
+			printf("#");
+
+		printf("  ");
+
+		for (int l = 0; l < row; l++)
+			printf("#");
+
+		printf("\n");
+	}
+
+	return 0;
 }
